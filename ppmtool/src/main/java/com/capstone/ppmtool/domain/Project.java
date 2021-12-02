be62608753650e6be528f59d970d62478f676a37
Justin Sienicki <justinsienicki@att.net>
2021-12-02 17:05:00 -0600

    Started Validation

--- ppmtool/src/main/java/com/capstone/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/capstone/ppmtool/domain/Project.java
@@ -1,6 +1,7 @@
 package com.capstone.ppmtool.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,8 +10,13 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "Project name is required")
 	private String projectName;
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min=4, max=5, message = "Please use 4 to 5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank
 	private String description;
 	private Date start_date;
 	private Date end_date;
