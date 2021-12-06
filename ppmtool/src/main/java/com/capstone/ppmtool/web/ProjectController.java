adedd3baa9ec890db6d8283fcf41d2579ed074c3
Justin Sienicki <justinsienicki@att.net>
2021-12-06 12:39:35 -0600

    Find Project by ID

--- ppmtool/src/main/java/com/capstone/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/capstone/ppmtool/web/ProjectController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,20 +28,28 @@ import java.util.Map;
 @RequestMapping("/api/project")
 public class ProjectController {
 
-    @Autowired
-    private ProjectService projectService;
+	@Autowired
+	private ProjectService projectService;
 
-    @Autowired
-    private MapValidationErrorService mapValidationErrorService;
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
 
+	@PostMapping("")
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-    @PostMapping("")
-    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if (errorMap != null)
+			return errorMap;
 
-        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if(errorMap!=null) return errorMap;
+		Project project1 = projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+	}
 
-        Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
-    }
+	@GetMapping("/{projectId}")
+	public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+
+		Project project = projectService.findProjectByIdentifier(projectId);
+
+		return new ResponseEntity<Project>(project, HttpStatus.OK);
+	}
 }
