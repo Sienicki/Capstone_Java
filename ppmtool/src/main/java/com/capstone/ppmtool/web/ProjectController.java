0566a02942b71d546f0f644c34ebb94419e8e961
Justin Sienicki <justinsienicki@att.net>
2021-12-06 13:41:00 -0600

    Find All Projects

--- ppmtool/src/main/java/com/capstone/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/capstone/ppmtool/web/ProjectController.java
@@ -52,4 +52,6 @@ public class ProjectController {
 
 		return new ResponseEntity<Project>(project, HttpStatus.OK);
 	}
+	@GetMapping("/all")
+    public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
 }
