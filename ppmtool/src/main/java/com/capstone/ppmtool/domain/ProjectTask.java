f0fd80fa5ae2fba60ca8ee1e828fa44feedd269d
Justin Sienicki <justinsienicki@att.net>
2021-12-21 09:39:50 -0600

    meta

--- ppmtool/src/main/java/com/capstone/ppmtool/domain/ProjectTask.java
+++ ppmtool/src/main/java/com/capstone/ppmtool/domain/ProjectTask.java
@@ -2,6 +2,9 @@ package com.capstone.ppmtool.domain;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Date;
 
 @Entity
@@ -19,6 +22,10 @@ public class ProjectTask {
     private Integer priority;
     private Date dueDate;
     //ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name="backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     @Column(updatable = false)
     private String projectIdentifer;
@@ -107,8 +114,17 @@ public class ProjectTask {
     public void setUpdate_At(Date update_At) {
         this.update_At = update_At;
     }
+    
 
-    @PrePersist
+    public Backlog getBacklog() {
+		return backlog;
+	}
+
+	public void setBacklog(Backlog backlog) {
+		this.backlog = backlog;
+	}
+
+	@PrePersist
     protected void onCreate(){
         this.create_At = new Date();
     }
